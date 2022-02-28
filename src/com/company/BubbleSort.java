24b5ecff47f3e1e68884c7f09bdbe101a579aed0
Shreya Rajput <rshreya24700@gmail.com>
2022-02-28 14:33:29 +0530

    variable change

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -7,22 +7,22 @@ public class BubbleSort {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the size of array: ");
         Integer n = scan.nextInt();
-        int []arr = new int[n];
+        int []array = new int[n];
         System.out.println("Enter the array elements: ");
         for(int i =0; i< n; i++){
-            arr[i] = scan.nextInt();
+            array[i] = scan.nextInt();
         }
         for (int i=0; i < n-1; i++){
             for (int j=0; j < n-i-1; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                 }
             }
         }
         for (int i=0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(array[i] + " ");
         }
     }
 
